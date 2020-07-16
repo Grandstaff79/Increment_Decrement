4711cde3e1ca0fe9d1a39d2b59756d8d4c283996
Grandstaff79 <48068304+Grandstaff79@users.noreply.github.com>
2020-07-16 16:45:17 -0400

    Add project files.

---
+++ Increment_Decrement/Increment_Decrement.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Increment_Decrement.Droid
+{
+    [Activity(Label = "Increment_Decrement",  Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+
+        TextView txtNumber;
+
+
+        int number;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (o, e) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (o, e) =>
+            txtNumber.Text = (--number).ToString();
+
+
+
+
+
+        }
+    }
+}
